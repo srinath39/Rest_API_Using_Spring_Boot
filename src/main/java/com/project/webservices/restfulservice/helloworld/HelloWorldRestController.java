bd0a36f95eb5fe808f936a57cf0c129fe80f0e7e
srinath39 <ashwinisrinath2580@gmail.com>
2026-04-29 23:25:34 +0530

    Learned Internalization in responses based on consumer requests.

--- src/main/java/com/project/webservices/restfulservice/helloworld/HelloWorldRestController.java
+++ src/main/java/com/project/webservices/restfulservice/helloworld/HelloWorldRestController.java
@@ -1,11 +1,23 @@
 package com.project.webservices.restfulservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldRestController {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldRestController(MessageSource messageSource){
+        this.messageSource=messageSource;
+    }
 
     @GetMapping(path ="/hello-world")
     public String helloWorld(){
@@ -20,6 +32,12 @@ public class HelloWorldRestController {
     @GetMapping(path ="/hello-world/path-variable/{name}")
     public HelloWorld helloWorldPathVariable(@PathVariable String name){
         return new HelloWorld(String.format("Hello world , %s",name));
+    }
+
+    @GetMapping(path ="/hello-world-internalization")
+    public String helloWorldInternalization(){
+        Locale locale= LocaleContextHolder.getLocale();
+        return messageSource.getMessage("GOOD_MORNING_MESSAGE_KEY",null,"Default text",locale);
     }
 
 }
