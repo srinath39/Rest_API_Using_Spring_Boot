58fe36631eebba71fc28c6672bc3ec5d0a4ce87b
srinath39 <ashwinisrinath2580@gmail.com>
2026-05-02 18:16:31 +0530

    Learned about Versioning- Query Parameter or Request Parameter versioning

--- src/main/java/com/project/webservices/restfulservice/versioning/VersionPersonController.java
+++ src/main/java/com/project/webservices/restfulservice/versioning/VersionPersonController.java
@@ -6,15 +6,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionPersonController {
 
-    // http://localhost:8080/version2/person
-
+    // http://localhost:8080/version1/person
     @GetMapping("/version1/person")
     public Person1 getFirstVersionPersonPathParameter(){
         return new Person1("Srinath Mangali");
     }
 
+    // http://localhost:8080/version2/person
     @GetMapping("/version2/person")
     public Person2 getSecondVersionPersonPathParameter(){
+        return new Person2(new Name("Srinath","Mangali"));
+    }
+
+    // http://localhost:8080/person?version=1
+    @GetMapping(path="/person",params ="version=1")
+    public Person1 getFirstVersionPersonQueryParameter(){
+        return new Person1("Srinath Mangali");
+    }
+
+    // http://localhost:8080/person?version=1
+    @GetMapping(path="/person",params ="version=2")
+    public Person2 getSecondVersionPersonQueryParameter(){
         return new Person2(new Name("Srinath","Mangali"));
     }
 
